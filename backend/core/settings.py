228c0ffa33026be9b9a93fc20ebc2d5c7d971d21
Yaroslav Sorochan <yaroslavsorocan89@gmail.com>
2025-11-07 20:43:59 +0200

    Update database settings to use PostgreSQL

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -81,10 +81,15 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'comments',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'db', # as defined in docker-compose.yml
+        'PORT': 5432,
     }
 }
+
 
 
 # Password validation
