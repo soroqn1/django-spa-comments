a53bbb008235f6fc8e858d4b8978a6fc21628abd
Yaroslav Sorochan <yaroslavsorocan89@gmail.com>
2025-11-12 12:43:28 +0200

    add ALLOWED_HOSTS

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -189,3 +189,5 @@ MEDIA_ROOT = BASE_DIR / 'media'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+ALLOWED_HOSTS = ['*']
